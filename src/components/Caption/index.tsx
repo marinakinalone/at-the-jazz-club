f5cc00b15f66674f5b2dd364035d06e136c47b29
Marina Kinalone Simonnet <marina.simonnet@trustly.com>
2025-05-19 13:32:19 +0100

    add typing effect

--- src/components/Caption/index.tsx
+++ src/components/Caption/index.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Caption = ({ message }: { message: string }) => {
-  // TODO add typing animation - see le journal
+  const [currentPosition, setCurrentPosition] = useState(0)
+  const items = message.split('')
+
+  useEffect(() => {
+    setCurrentPosition(0)
+  }, [message])
+
+  useEffect(() => {
+    if (currentPosition >= items.length) return
+
+    const intervalId = setInterval(() => {
+      setCurrentPosition((prevPosition) => prevPosition + 1)
+    }, 25)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [currentPosition, items])
+
+  const animatedMessage = items.slice(0, currentPosition).join('')
+
   return (
     <div>
-      <p>{message}</p>
+      <p>{animatedMessage}</p>
     </div>
   )
 }
